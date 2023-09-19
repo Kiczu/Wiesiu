87514f1795d4419e8ca06f143fb4a159ae398644
Kiczu <adrian.mamyto@gmail.com>
2023-09-19 21:35:40 +0200

    Delete buttons and styled-components

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import MainButton from "../components/Buttons/MainButton/MainButton";
 
 const Home = () => {
   return (
     <div style={{ height: 300 + "vh" }}>
       Home
-      <MainButton>Więcej</MainButton>
     </div>
   );
 };
