66820c46736b55d8f67be29e57d4d09a919803a9
Kiczu <adrian.mamyto@gmail.com>
2023-10-12 02:27:10 +0200

    Add ENV to InstagramFeed

--- src/services/instagramService.js
+++ src/services/instagramService.js
@@ -4,8 +4,7 @@ const getPosts = (limit) => {
   return api.get(endpoints.instagram.media, {
     fields: "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp",
     limit,
-    access_token:
-      "IGQWRNei12SlFIeTgyYUdJYXNyTEJrSU5zeWdGQlRCVENrNF84YkhybWhXQ2lGR3B1TjdxTTEwNExqbXBreUV3SG03VmoxR0Q0bDRiM2FNTzZAPaHFFMkh2QVR3VkRHYy1rMjRRZAEwyNVlGWkg5WVlDbHFSQl9DVjgZD",
+    access_token: process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN,
   });
 };
 
