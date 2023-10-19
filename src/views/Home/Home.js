d6f98f87ca42b87a373cf3b439c1848e9ae5fe31
Kiczu <adrian.mamyto@gmail.com>
2023-10-19 03:10:47 +0200

    Add sections to HomePage components

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,13 +1,36 @@
 import React from "react";
+import useuseWooCommerceProducts from "../../hooks/useWooCommerceProducts";
 import SectionHero from "./SectionHero/SectionHero";
-import "./Home.scss";
 import SectionMeetings from "./SectionMeetings/SectionMeeting";
+import SectionSlider from "./SectionSlider/SectionSlider";
+import InstagramFeed from "../../components/InstagramFeed/InstagramFeed";
+import "./Home.scss";
 
 const Home = () => {
+  const comics = useuseWooCommerceProducts(18);
+  const collections = useuseWooCommerceProducts(23);
+  const gadgets = useuseWooCommerceProducts(19);
+
   return (
     <>
       <SectionHero />
+      <SectionSlider
+        products={comics}
+        sectionTitle={"Księgarnia"}
+        buttonText={"Do sklepu"}
+      />
       <SectionMeetings />
+      <SectionSlider
+        products={collections}
+        sectionTitle={"Kolekcje"}
+        buttonText={"Do sklepu"}
+      />
+      <SectionSlider
+        products={gadgets}
+        sectionTitle={"Gadżety"}
+        buttonText={"Do sklepu"}
+      />
+      <InstagramFeed />
     </>
   );
 };
