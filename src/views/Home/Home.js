86be70cee104f177b2f617737746834f65854275
Kiczu <adrian.mamyto@gmail.com>
2023-10-19 02:09:28 +0200

    Add SectionMeetings component

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SectionHero from "./SectionHero/SectionHero";
 import "./Home.scss";
+import SectionMeetings from "./SectionMeetings/SectionMeeting";
 
 const Home = () => {
   return (
     <>
       <SectionHero />
+      <SectionMeetings />
     </>
   );
 };
