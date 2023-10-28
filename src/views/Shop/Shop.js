7b04577f4adaa775126c73e6feb6191b6a321d43
Kiczu <adrian.mamyto@gmail.com>
2023-10-28 13:16:44 +0200

    Add select

--- src/views/Shop/Shop.js
+++ src/views/Shop/Shop.js
@@ -11,38 +11,51 @@ const Shop = () => {
   return (
     <>
       <SectionTitle>Sklep</SectionTitle>
-      <div className="categories-filter">
-        {categories.map((category, i) => (
-          <button
-            className={`categories-filter-button ${
-              Number(activeCategory) === category.id ? "active" : ""
-            }`}
-            key={i}
-            data-category-id={category.id}
-            onClick={(e) => handleCategoryClick(e)}
-          >
-            {category.name}
-          </button>
-        ))}
-      </div>
       <div className="listing-products-container">
-        <ul className="listing-products-grid">
-          {products.map((product, i) => (
-            <li className="listing-products-grid-item" key={i}>
-              <img
-                className="product-image"
-                src={
-                  product.images.length
-                    ? product.images[0].src
-                    : imagePlaceholder
-                }
-                alt=""
-              />
-              <h2 className="product-title">{product.name}</h2>
-              <p className="product-price">{product.price} zł</p>
-            </li>
+        <div className="left-column categories-filter">
+          {categories.map((category, i) => (
+            <button
+              className={`categories-filter-button ${
+                Number(activeCategory) === category.id
+                  ? "active-category-filter"
+                  : ""
+              }`}
+              key={i}
+              data-category-id={category.id}
+              onClick={(e) => handleCategoryClick(e)}
+            >
+              {category.name}
+            </button>
           ))}
-        </ul>
+        </div>
+        <div className="right-column">
+          <div className="sorting-filter">
+            <div className="custom-select">
+              <select>
+                <option value="date">Data dodania</option>
+                <option value="price">Cena</option>
+                <option value="popularity">Popularność</option>
+              </select>
+            </div>
+          </div>
+          <ul className="listing-products-grid">
+            {products.map((product, i) => (
+              <li className="listing-products-grid-item" key={i}>
+                <img
+                  className="product-image"
+                  src={
+                    product.images.length
+                      ? product.images[0].src
+                      : imagePlaceholder
+                  }
+                  alt={product.name}
+                />
+                <h2 className="product-title">{product.name}</h2>
+                <p className="product-price">{product.price} zł</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
