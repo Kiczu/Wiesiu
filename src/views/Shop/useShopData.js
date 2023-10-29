106a2c6bb2ba73ea44ee6ec4e80f2c5af7ae394e
Kiczu <adrian.mamyto@gmail.com>
2023-10-29 14:34:17 +0100

    Add category filter - if 0 products dont show cat, sort function instead fetching

--- src/views/Shop/useShopData.js
+++ src/views/Shop/useShopData.js
@@ -5,12 +5,14 @@ const useShopData = () => {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [activeCategory, setActiveCategory] = useState(null);
+  const [productsByCategory, setProductsByCategory] = useState({});
 
   useEffect(() => {
     const getAllProducts = async () => {
       try {
         const data = await woocommerceServices.getProducts();
         setProducts(data);
+        setProductsByCategory(sortProductsByCategory(data));
       } catch (error) {
         console.error(error);
       }
@@ -22,7 +24,8 @@ const useShopData = () => {
     const getCategories = async () => {
       try {
         const data = await woocommerceServices.getCategories();
-        setCategories(data);
+        const filteredCategories = data.filter((category) => category.count !== 0);
+        setCategories(filteredCategories);
       } catch (error) {
         console.error(error);
       }
@@ -30,21 +33,24 @@ const useShopData = () => {
     getCategories();
   }, []);
 
+  const sortProductsByCategory = (products) => {
+    const sortedProducts = {};
+    products.forEach((product) => {
+      const { categories } = product;
+      categories.forEach((category) => {
+        if (!sortedProducts[category.id]) {
+          sortedProducts[category.id] = [];
+        }
+        sortedProducts[category.id].push(product);
+      });
+    });
+    return sortedProducts;
+  };
+
   const handleCategoryClick = (e) => {
     const categoryId = e.target.getAttribute("data-category-id");
-    const getFilteredProducts = async () => {
-      try {
-        const data = await woocommerceServices.getProductsByCategoryId(
-          categoryId
-        );
-        setProducts(data);
-        setActiveCategory(categoryId);
-        console.log(categoryId);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getFilteredProducts();
+    setActiveCategory(categoryId);
+    setProducts(productsByCategory[categoryId]);
   };
 
   return {
